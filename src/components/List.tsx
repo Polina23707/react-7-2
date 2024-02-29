1ff68b24f7426e9a6acbddb4f8f206634c995c14
Polina Gorelik <Polina23707@gmail.com>
2024-02-29 19:50:30 +0200

    fixed style

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,6 @@
-import Video, {VideoType} from "./Video";
-import { Article } from "./Article";
+import VideoWithFrame from "./VideoWithFrame";
+import ArticleWithFrame from "./ArticleWithFrame";
+import { VideoType } from "./Video";
 
 interface ListType {
   list: VideoType[],
@@ -7,16 +8,16 @@ interface ListType {
 
 export default function List({...props}: ListType) {
   return props.list.map(item => {
-      switch (item.type) {
-          case 'video':
-              return (
-                  <Video key={item.views} {...item}/>
-              );
+    switch (item.type) {
+      case 'video':
+        return (
+          <VideoWithFrame key={item.views} {...item} />
+        );
 
-          case 'article':
-              return (
-                  <Article key={item.title} {...item} />
-              );
+      case 'article':
+        return (
+          <ArticleWithFrame key={item.title} {...item} />      
+        );
       }
   });
 };
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,14 +2,15 @@ export interface VideoType {
   type?: string,
   url?: string,
   title?: string,
-  views: number,
+  views?: number,
 }
 
- export default function Video({url, views}: VideoType) {
+ export default function Video({...props}: VideoType) {
   return (
-      <div className="item item-video">
-          <iframe src={url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
-          <p className="views">Просмотров: {views}</p>
-      </div>
+    <div className="item item-video">
+      <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
   )
 };
+
